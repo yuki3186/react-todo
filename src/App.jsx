848d9a763335ed30e4cf7503a9037d2550d5d9fe
yuki3186 <67695166+yuki3186@users.noreply.github.com>
2021-06-06 11:12:54 +0900

    6/6

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,41 @@
-/* eslint react-hooks/exhaustive-deps: off */
 import React from "react";
 import "./styles.css";
 
 export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState([
+    "ああああ",
+    "いいいいい"
+    ]);
+
   return (
     <>
-      <div>
+      <div className="input-area">
         <input placeholder="TODOを入力" />
         <button>追加</button>
       </div>
-      <div>
-        <p>未完了のTODO</p>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
         <ul>
-          <div>
-            <li>あああああ</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
-          <div>
-            <li>いいいいい</li>
-            <button>完了</button>
-            <button>削除</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>完了</button>
+                <button>削除</button>
+              </div>
+            );
+          })}
+        
         </ul>
       </div>
-      <div>
-        <p>完了のTODO</p>
+      <div className="complete-area">
+        <p className="title">完了のTODO</p>
         <ul>
-          <div>
+          <div className="list-row">
             <li>あああああ</li>
             <button>戻す</button>
           </div>
-          <div>
+          <div className="list-row">
             <li>いいいいい</li>
             <button>戻す</button>
           </div>
